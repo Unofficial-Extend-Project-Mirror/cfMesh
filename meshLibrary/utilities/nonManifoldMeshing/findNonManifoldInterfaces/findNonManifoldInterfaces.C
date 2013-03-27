9824296009121999e53a95025c353ff159da21e7
franjo <franjo@fdcce57e-7e00-11e2-b579-49867b4cea03>
2013-03-27 22:28:28 +0000

    Progress towards multi-material meshing
    git-svn-id: https://pl5.projectlocker.com/igui/meshGeneration/svn@7 fdcce57e-7e00-11e2-b579-49867b4cea03

--- meshLibrary/utilities/nonManifoldMeshing/findNonManifoldInterfaces/findNonManifoldInterfaces.C
+++ meshLibrary/utilities/nonManifoldMeshing/findNonManifoldInterfaces/findNonManifoldInterfaces.C
@@ -29,7 +29,10 @@ Description
 #include "findNonManifoldInterfaces.H"
 #include "meshOctree.H"
 #include "findCellsIntersectingSurface.H"
+#include "triSurf.H"
 #include "demandDrivenData.H"
+
+#define DEBUGNonManifoldInterfaces
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -40,14 +43,169 @@ namespace Foam
 
 void findNonManifoldInterfaces::findIntersectedCells()
 {
+    Info << "Starting finding cells intersected by the surface" << endl;
+
     findCellsIntersectingSurface cis(mesh_, octree_);
 
-    intersectedCells_ = cis.facetsIntersectingCells();
+    const VRWGraph& facetsIntersectedCells = cis.facetsIntersectingCells();
+
+    //- find which surface patches intersect a cell
+    const triSurf& surface = octree_.surface();
+    patchesIntersectingCells_.clear();
+    patchesIntersectingCells_.setSize(facetsIntersectedCells.size());
+
+    forAll(facetsIntersectedCells, cellI)
+    {
+        forAllRow(facetsIntersectedCells, cellI, i)
+        {
+            const label triI = facetsIntersectedCells(cellI, i);
+            patchesIntersectingCells_.appendIfNotIn
+            (
+                cellI,
+                surface[triI].region()
+            );
+        }
+    }
+
+    # ifdef DEBUGNonManifoldInterfaces
+    Map<label> regionToID;
+    forAll(patchesIntersectingCells_, cellI)
+    {
+        forAllRow(patchesIntersectingCells_, cellI, i)
+        {
+            const label regionI = patchesIntersectingCells_(cellI, i);
+            Map<label>::iterator iter = regionToID.find(regionI);
+            if( iter == regionToID.end() )
+            {
+                const word sName =
+                    "intersected_"+mesh_.boundaries()[regionI].patchName();
+                regionToID.insert
+                (
+                    regionI,
+                    mesh_.addCellSubset(sName)
+                );
+
+                iter = regionToID.find(regionI);
+            }
+
+            mesh_.addCellToSubset(iter(), cellI);
+        }
+    }
+    # endif
+
+    Info << "Finished finding cells intersected by the surface" << endl;
 }
 
 void findNonManifoldInterfaces::findNeighbouringGroups()
 {
+    //- start assigning neighbouring cells of the intersected cells into groups
+    neiGroups_.setSize(0);
+    neiGroups_.setSize(patchesIntersectingCells_.size());
+    const labelList& owner = mesh_.owner();
+    const labelList& neighbour = mesh_.neighbour();
 
+    for(label fI=0;fI<mesh_.nInternalFaces();++fI)
+    {
+        const label own = owner[fI];
+        const label nei = neighbour[fI];
+
+        if( patchesIntersectingCells_.sizeOfRow(own) )
+        {
+            forAllRow(patchesIntersectingCells_, own, i)
+            {
+                const label patchI = patchesIntersectingCells_(own, i);
+
+                if( !patchesIntersectingCells_.contains(nei, patchI) )
+                    neiGroups_.append(nei, patchI);
+            }
+        }
+        else if( patchesIntersectingCells_.sizeOfRow(nei) )
+        {
+            forAllRow(patchesIntersectingCells_, nei, i)
+            {
+                const label patchI = patchesIntersectingCells_(nei, i);
+
+                if( !patchesIntersectingCells_.contains(own, patchI) )
+                    neiGroups_.append(own, patchI);
+            }
+        }
+    }
+
+    # ifdef DEBUGNonManifoldInterfaces
+    Map<label> regionToID;
+    forAll(neiGroups_, cellI)
+    {
+        forAllRow(neiGroups_, cellI, i)
+        {
+            const label regionI = neiGroups_(cellI, i);
+            Map<label>::iterator iter = regionToID.find(regionI);
+            if( iter == regionToID.end() )
+            {
+                const word sName =
+                    "neighboursOfPatch_" +
+                    mesh_.boundaries()[regionI].patchName();
+
+                regionToID.insert
+                (
+                    regionI,
+                    mesh_.addCellSubset(sName)
+                );
+
+                iter = regionToID.find(regionI);
+            }
+
+            mesh_.addCellToSubset(iter(), cellI);
+        }
+    }
+    # endif
+}
+
+bool findNonManifoldInterfaces::findFaceCandidates()
+{
+    internalFacePatches_.clear();
+    internalFacePatches_.setSize(mesh_.nInternalFaces());
+
+    const labelList& owner = mesh_.owner();
+    const labelList& neighbour = mesh_.neighbour();
+
+    forAll(internalFacePatches_, faceI)
+    {
+        const label own = owner[faceI];
+        const label nei = neighbour[faceI];
+
+        forAllRow(patchesIntersectingCells_, own , i)
+        {
+            const label patchI = patchesIntersectingCells_(own, i);
+
+            if(
+                !patchesIntersectingCells_.contains(nei, patchI) &&
+                neiGroups_.contains(nei, patchI)
+            )
+                internalFacePatches_.appendIfNotIn(faceI, patchI);
+        }
+
+        forAllRow(patchesIntersectingCells_, nei , i)
+        {
+            const label patchI = patchesIntersectingCells_(nei, i);
+
+            if(
+                !patchesIntersectingCells_.contains(own, patchI) &&
+                neiGroups_.contains(own, patchI)
+            )
+                internalFacePatches_.appendIfNotIn(faceI, patchI);
+        }
+    }
+
+    //- check if there exist any proximity problems
+    forAll(internalFacePatches_, faceI)
+    {
+        if( internalFacePatches_.sizeOfRow(faceI) < 2 )
+            continue;
+
+        //- check if it is a proximity problem
+    }
+
+    return false;
 }
 
 bool findNonManifoldInterfaces::extractInterfaces()
@@ -59,7 +217,7 @@ bool findNonManifoldInterfaces::extractInterfaces()
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-// Construct from mesh, octree, regions for boundary vertices
+// Construct from mesh and octree
 findNonManifoldInterfaces::findNonManifoldInterfaces
 (
     polyMeshGen& mesh,
@@ -67,7 +225,10 @@ findNonManifoldInterfaces::findNonManifoldInterfaces
 )
 :
     mesh_(mesh),
-    octree_(octree)
+    octree_(octree),
+    patchesIntersectingCells_(),
+    neiGroups_(),
+    internalFacePatches_()
 {}
 
 // * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
@@ -79,7 +240,20 @@ findNonManifoldInterfaces::~findNonManifoldInterfaces()
 
 void findNonManifoldInterfaces::createNonManifoldInterfaces()
 {
+    do
+    {
+        //- find cells intersected by the surface
+        findIntersectedCells();
 
+        //- find cells near the intersected cells
+        findNeighbouringGroups();
+    } while( findFaceCandidates() );
+
+    if( extractInterfaces() )
+        WarningIn
+        (
+            "void findNonManifoldInterfaces::createNonManifoldInterfaces()"
+        ) << "Could not generate all internal interfaces" << endl;
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
