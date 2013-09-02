db385fe4fc5f34b88ddeef505693068f988d6380
franjo_j@hotmail.com <franjo_j@hotmail.com@fdcce57e-7e00-11e2-b579-49867b4cea03>
2013-09-02 21:58:34 +0000

    Added an utility for removing selected parts of the surface mesh
    git-svn-id: https://pl5.projectlocker.com/igui/meshGeneration/svn@23 fdcce57e-7e00-11e2-b579-49867b4cea03

---
+++ utilities/removeSurfaceFacets/removeSurfaceFacets.C
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2005-2007 Franjo Juretic
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+Description
+    Reads the surface mesh, remove the selected facets
+    and writes the modified mesh into a new file
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "Time.H"
+#include "triSurf.H"
+#include "triSurfaceRemoveFacets.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::noParallel();
+    argList::validArgs.clear();
+
+    argList::validArgs.append("input surface file");
+    argList::validArgs.append("output surface file");
+    argList::validArgs.append("Subset/patch name");
+
+    argList args(argc, argv);
+
+    fileName inFileName(args.args()[1]);
+    fileName outFileName(args.args()[2]);
+    word patchName(args.args()[3]);
+
+    //- read the input surface
+    triSurf origSurf(inFileName);
+
+    //- remove the selected facets
+    triSurfaceRemoveFacets rsf(origSurf);
+
+    rsf.selectFacetsInPatch(patchName);
+
+    rsf.removeFacets();
+
+    //- write the modified surface mesh
+    origSurf.writeSurface(outFileName);
+
+    Info << "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
