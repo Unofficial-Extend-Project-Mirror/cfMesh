6a38fe040641a1f287f512ee277f95701be0a3c8
Franjo Juretic <franjo.juretic@c-fields.com>
2015-02-16 12:48:02 +0100

    Changes in meshEdgeExtractorNonTopo - it does not assign patches any more

--- meshLibrary/utilities/surfaceTools/meshSurfaceEdgeExtractorNonTopo/meshSurfaceEdgeExtractorNonTopoDistributeFaces.C
+++ meshLibrary/utilities/surfaceTools/meshSurfaceEdgeExtractorNonTopo/meshSurfaceEdgeExtractorNonTopoDistributeFaces.C
@@ -27,16 +27,15 @@ Description
 
 #include "demandDrivenData.H"
 #include "meshSurfaceEdgeExtractorNonTopo.H"
-#include "meshOctree.H"
-#include "triSurf.H"
+//#include "meshOctree.H"
+//#include "triSurf.H"
 #include "meshSurfaceEngine.H"
 #include "meshSurfaceMapper.H"
-#include "helperFunctions.H"
-#include "edgeExtractor.H"
+//#include "helperFunctions.H"
+//#include "edgeExtractor.H"
 
-# ifdef USE_OMP
-#include <omp.h>
-# endif
+#include "correctEdgesBetweenPatches.H"
+
 
 //#define DEBUGMapping
 
@@ -47,14 +46,9 @@ namespace Foam
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-void meshSurfaceEdgeExtractorNonTopo::distributeBoundaryFaces()
+void meshSurfaceEdgeExtractorNonTopo::decomposeBoundaryFaces()
 {
-    edgeExtractor extractor(mesh_, meshOctree_);
-
-    Info << "Extracting edges" << endl;
-    extractor.extractEdges();
-
-    extractor.updateMeshPatches();
+    correctEdgesBetweenPatches featureEdges(mesh_);
 }
 
 void meshSurfaceEdgeExtractorNonTopo::remapBoundaryPoints()
