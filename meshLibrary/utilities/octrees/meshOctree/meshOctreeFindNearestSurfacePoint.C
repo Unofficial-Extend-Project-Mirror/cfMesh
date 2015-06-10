d5405537408c4c962668e8ab158b0cacf005626c
Franjo Juretic <franjo.juretic@c-fields.com>
2015-06-10 12:18:42 +0200

    Increased the number of iterations for finding the nearest surface point

--- meshLibrary/utilities/octrees/meshOctree/meshOctreeFindNearestSurfacePoint.C
+++ meshLibrary/utilities/octrees/meshOctree/meshOctreeFindNearestSurfacePoint.C
@@ -107,7 +107,7 @@ void meshOctree::findNearestSurfacePoint
         if( !found )
             sizeVec *= 2.0;
 
-    } while( !found /*&& (iterationI++ < 5)*/ );
+    } while( !found && (iterationI++ < 100) );
 
     # ifdef DEBUGSearch
     forAll(surface_, triI)
